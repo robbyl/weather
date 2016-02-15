1491f56893360817650bcb12ef0e62b4500a552d
Robert <robbyl@ovi.com>
2016-02-15 12:23:34 +0300

    added config.xml file

--- manifest.php
+++ manifest.php
@@ -5,9 +5,9 @@ $filesToCache = array(
     './js/weather7.js', 
     './css/weather7.css', 
     './img/yahoo-logo.png', 
-    '../../dist/js/framework7.min.js', 
-    '../../dist/css/framework7.material.min.css'
-    '../../dist/css/framework7.material.colors.min.css'
+    'dist/js/framework7.min.js', 
+    'dist/css/framework7.material.min.css',
+    'dist/css/framework7.material.colors.min.css'
 );
 ?>
 CACHE MANIFEST
